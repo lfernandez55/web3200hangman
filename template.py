82fc337f40c6c9aa917d329bf69b59a05e08116c
lfernandez55 <lfernandez@weber.edu>
2020-11-07 19:40:44 -0700

    Commit

---
+++ template.py
@@ -0,0 +1,24 @@
+# TODO: Import the random module
+
+
+# TODO: Create a list that contains 15 words (with varying lengths) as strings (use this to randomly 
+# create the word_to_guess
+
+
+# TODO: Define a function called `create_initial_hint` that takes one parameter:
+# word_to_guess ( a list that represents the word to guess, for example: ['b','l','a','c','k']   )
+# after processing the input the function should return the initial hint (for example
+# given the above input the function should return  ['_','_','_','_','_'] )
+
+
+# TODO: Define a function called `create_hint` that takes three parameters:
+# word_to_guess ( a list that represents the word to guess, for example: ['b','l','a','c','k']   )
+# hint( a list that represents the hint so far, for example:    ['b','_','a','_','_'] )
+# guess ( a string that represents the guess, for example: 'c'
+# after processing the input the function should return the hint (for example given 
+# the above input the function should return ['b','_','a','c','_'])
+
+
+# TODO After creating the above functions and verifying they work create the rest of the program using,
+# among other things, a while loop
+
